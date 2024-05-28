1ec8618a826f11291a05112eb68eef3d20e922e9
muhammadali23 <muhammadali121999@gmail.com>
2024-05-28 22:40:43 +0500

    Assignment_03

---
+++ C5MA_React/Assignment_03/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./Header";
+import Content from "./Content";
+function App() {
+  return (
+    <>
+      <Header name="Header" />
+      <Content name="Content" />
+    </>
+  );
+}
+
+export default App;
---
+++ C5MA_React/Assignment_03/src/Content.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Content(props) {
+  return (
+    <div>
+      <h1>This is {props.name}</h1>
+    </div>
+  );
+}
+
+export default Content;
