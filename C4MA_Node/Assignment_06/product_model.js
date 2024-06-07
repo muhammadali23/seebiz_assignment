ed1932e3f5b0ab1c9148d86621af540a797f6dcb
muhammadali23 <muhammadali121999@gmail.com>
2024-06-07 11:17:50 +0500

    assignment 05

---
+++ C4MA_Node/Assignment_06/product_model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const { type } = require("os");
+// const { type } = require("os");
+// const { boolean } = require("webidl-conversions");
+
+const productSchema = new mongoose.Schema({
+  product_name: {
+    type: String,
+    min: 15,
+    max: 30,
+  },
+  catagory: String,
+  quantity: Number,
+  price: Number,
+  attribute: {
+    color: String,
+    material: String,
+  },
+  group_price: [
+    {
+      quantity: Number,
+      price: Number,
+    },
+  ],
+  brand: {
+    type: String,
+    default: null,
+  },
+  available_status: boolean,
+  CreatedAt: {
+    type: Date,
+    default: () => Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: () => Date.now(),
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
